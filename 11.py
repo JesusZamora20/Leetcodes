905e24f2ffd245dab0e6c46fe7f83dc8bdeabe55
Jesús Bermudez Zamora <99784430+JesusZamora20@users.noreply.github.com>
2025-02-06 15:58:50 -0500

    Update 11.py -> I found an optimzed time complexity solution

--- 11.py
+++ 11.py
@@ -8,13 +8,17 @@ Created on Fri Oct  4 00:27:43 2024
 # 11
 
 class Solution(object):
-    def maxArea(self, height):
-        
+    def maxArea(self, height: List[int]) -> int:
         maxwater = 0
+        p1 = 0
+        p2 = len(height) -1
 
-        for p1 in range(len(height)):
-            for p2 in range(len(height)):
-                water = min([height[p1], height[p2]]) * (p2-p1)
-                if water > maxwater: 
-                    maxwater = water
+        while p1 < p2:
+            current_water = min([height[p1], height[p2]]) * (p2-p1)
+            if current_water > maxwater:
+                maxwater = current_water
+            elif height[p1] < height[p2]:
+                p1 += 1
+            else:
+                p2 -= 1
         return maxwater
